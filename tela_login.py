f049e71413a4413161a2f9a796d0887e6a9cc776
ImpactPark <gp.prjimpacta@gmail.com>
2023-03-22 22:38:31 -0300

    commit

--- tela_login.py
+++ tela_login.py
@@ -37,13 +37,12 @@ def cadastrar_usuario():
 # problema foi criado um TRY e um EXCEPT para captura destes erros
     if (senha == confirma_senha):
         try:
-            banco = sqlite3.connect('banco_cadastro.db')
-            cursor = banco.cursor()
-            cursor.execute("CREATE TABLE IF NOT EXISTS cadastro_usuario(nome text,login text,senha text)")
-            cursor.execute("INSERT INTO cadastro_usuario VALUES ('"+nome+"', '"+login+"', '"+senha+"')")
-
-            banco.commit()
-            banco.close()
+            banco = sqlite3.connect('banco_cadastro.db') #Função do sqlite para criação do banco através do connect caso o banco ja exista esta linha não serpa executada
+            cursor = banco.cursor() # Cursor usado para manipulação do banco e criação das querys
+            cursor.execute("CREATE TABLE IF NOT EXISTS cadastro_usuario(nome text,login text,senha text)") # Criação da tabela seguido das respectivas colunas para armazenar os dados dos usuários cadastrados 
+            cursor.execute("INSERT INTO cadastro_usuario VALUES ('"+nome+"', '"+login+"', '"+senha+"')") # Será inserido no banco o que foi digitado nas variáveis criadas na função cadastrar_usuario
+            banco.commit() #Commit para salvar as alteraçõs no banco 
+            banco.close() # Fechando a conexão com o banco de dados
             tela_cadastro_usuario.label_2.setText("Usuário cadastrado com sucesso!")
 
         except sqlite3.Error as erro:
@@ -55,13 +54,13 @@ def cadastrar_usuario():
 
 
 
-app = QtWidgets.QApplication([])
-tela_login = uic.loadUi("tela_login.ui")
-menu_principal = uic.loadUi("menu_principal.ui")
-tela_cadastro_usuario = uic.loadUi("tela_cadastro_usuario.ui")
-tela_login.pushButton_2.clicked.connect(chama_menu_principal)
-menu_principal.pushButton_3.clicked.connect(sair)
-tela_login.lineEdit_2.setEchoMode(QtWidgets.QLineEdit.Password)
+app = QtWidgets.QApplication([]) # Objeto app que utiliza a classe QtWidgets para criar a aplicação
+tela_login = uic.loadUi("tela_login.ui") # Criação do objeto formulario que utiliza o uic para carregar/importar o arquivo "tela_login.ui" 
+menu_principal = uic.loadUi("menu_principal.ui") # Criação do objeto formulario que utiliza o uic para carregar/importar o arquivo "menu_principal.ui" 
+tela_cadastro_usuario = uic.loadUi("tela_cadastro_usuario.ui")  
+tela_login.pushButton_2.clicked.connect(chama_menu_principal) 
+menu_principal.pushButton_3.clicked.connect(sair) 
+tela_login.lineEdit_2.setEchoMode(QtWidgets.QLineEdit.Password) # Propriedade setEchoMode utilizada para esconder as senhas digitadas no formulário
 tela_login.pushButton_3.clicked.connect(abre_tela_cadastro)
 tela_cadastro_usuario.pushButton_2.clicked.connect(cadastrar_usuario)
 
